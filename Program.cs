78a7912ff479c681c951b22b32a688135f423e56
kaiorod <caiorodrigo2775@gmail.com>
2021-09-12 22:49:49 -0300

    Finaliza GrausFC

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, convc;
+
+            Console.Clear();
+            Console.Beep();
+            Console.Write("Digite a temperatura em ºF : ");
+            f = double.Parse(Console.ReadLine());
+
+            convc = (f - 32) / 1.8;
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"{convc} ºC"); 
+            Console.ResetColor();          
+        }
+    }
+}
